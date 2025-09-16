f6499bf2cb62948f310417989ad173533625fa0e
LAKSHMI NARAYANAN K Y <kylakshminarayanan@gmail.com>
2025-09-16 06:51:31 +0000

    change fonts to futuristic (_for element h2_)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['PT Sans', 'sans-serif'],
-        headline: ['Playfair Display', 'serif'],
+        body: ['"Exo 2"', 'sans-serif'],
+        headline: ['Orbitron', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
